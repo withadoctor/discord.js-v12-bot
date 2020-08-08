15d901909cc4c24fe9a16cea94f575503a75e128
alex - windows <alicelukas@naver.com>
2020-08-09 06:06:58 +0900

    update covid

--- commands/miscellaneous/covid19.js
+++ commands/miscellaneous/covid19.js
@@ -1,8 +1,10 @@
 const cheerio = require("cheerio")
 const request = require("request")
 
-// let url = "http://ncov.mohw.go.kr/" //코로나바이러스감염증-19(COVID-19)
 let url = "http://ncov.mohw.go.kr/bdBoardList_Real.do?brdId=1&brdGubun=11&ncvContSeq=&contSeq=&board_id=&gubun=" //코로나바이러스감염증-19(COVID-19)
+let aa = -1;
+var covid_notice_channel;
+
 module.exports = { 
     config: {
         name: "covid",
@@ -12,28 +14,39 @@ module.exports = {
         category: "miscellaneous", // help에서 표시 하지 않음
         accessableby: "Members" // help에서 표시 하지 않음
     },
+    core: async () => {
+        return new Promise((resolve, reject) => {
+            request(url, (error, response, body) => {
+                const $ = cheerio.load(body)
+                const livedate = $("h5.s_title_in3 span.t_date");
+                const confirmedCase = $("li p.inner_value");
+            
+                let info = "누적 확진자 현황 : \`\`" + livedate[0].children[0].data + "\`\`";
+                let text1 = "신규 확진자 : \`\`" + confirmedCase[0].children[0].data + "\`\`";
+                let text2 = "해외유입 : \`\`" + confirmedCase[1].children[0].data + "\`\`";
+                let text3 = "국내발생 : \`\`" + confirmedCase[2].children[0].data + "\`\`";
+            
+                resolve(`${info}\n\n${text1}\n${text2}\n${text3}`);
+            })
+        });
+    },
+    crun: async (bot) => {
+        if(aa == -1) {
+            bot.guilds.cache.forEach(x => {
+                if(x.name == '__youtube_test') { // 나긋해_유튜브_채널
+                    x.channels.cache.forEach(y => {
+                        if(y.name == '테스트') {
+                            aa = y.id;
+                        }
+                    })
+                }
+            });
+            covid_notice_channel = bot.channels.cache.find(x => x.id == aa);
+        }
+
+        covid_notice_channel.send(await module.exports.core());
+    },
     run: async (bot, message, args) => {
-        // await request(url, function(error, response, body) { // "http://ncov.mohw.go.kr/"
-        //     const $ = cheerio.load(body)
-        //     const livedate = $(".livedate");
-        //     const colArr = $("li span.before");
-
-        //     let text1 = "환자 현황 " + livedate[0].children[0].data;
-        //     let text2 = "신규 확진자" + colArr[0].children[0].data.slice(4);
-
-        //     message.channel.send(`${text1}\n\n${text2}`)
-        // })
-        await request(url, function(error, response, body) {
-            const $ = cheerio.load(body)
-            const livedate = $("h5.s_title_in3 span.t_date");
-            const confirmedCase = $("li p.inner_value");
-
-            let info = "누적 확진자 현황 : \`\`" + livedate[0].children[0].data + "\`\`";
-            let text1 = "신규 확진자 : \`\`" + confirmedCase[0].children[0].data + "\`\`";
-            let text2 = "해외유입 : \`\`" + confirmedCase[1].children[0].data + "\`\`";
-            let text3 = "국내발생 : \`\`" + confirmedCase[2].children[0].data + "\`\`";
-
-            message.channel.send(`${info}\n\n${text1}\n${text2}\n${text3}`)
-        })
+        message.channel.send(await module.exports.core())
     }
 }
