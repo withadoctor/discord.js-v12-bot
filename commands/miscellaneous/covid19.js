8d309fe4ca7d3f0eba32ae9199bd6c970fade5ef
alex - windows <alicelukas@naver.com>
2020-08-15 23:29:51 +0900

    update covid19.js
    이전 기록까지 추가

--- commands/miscellaneous/covid19.js
+++ commands/miscellaneous/covid19.js
@@ -8,25 +8,44 @@ var covid_notice_channel;
 module.exports = { 
     config: {
         name: "covid",
-        aliases: ["co", "코로나"],
+        aliases: ["co", "코로나", "cv"],
         description: "대한민국 코로나 상태",
         usage: "covid",
         category: "miscellaneous", // help에서 표시 하지 않음
         accessableby: "Members" // help에서 표시 하지 않음
     },
-    core: async () => {
+    core: async (bot) => {
         return new Promise((resolve, reject) => {
             request(url, (error, response, body) => {
                 const $ = cheerio.load(body)
                 const livedate = $("h5.s_title_in3 span.t_date");
                 const confirmedCase = $("li p.inner_value");
             
-                let info = "누적 확진자 현황 : \`\`" + livedate[0].children[0].data + "\`\`";
-                let text1 = "신규 확진자 : \`\`" + confirmedCase[0].children[0].data + "\`\`";
-                let text2 = "해외유입 : \`\`" + confirmedCase[1].children[0].data + "\`\`";
-                let text3 = "국내발생 : \`\`" + confirmedCase[2].children[0].data + "\`\`";
+                let day_date = livedate[0].children[0].data;
+                let day_all = confirmedCase[0].children[0].data;
+                let day_oversea = confirmedCase[1].children[0].data;
+                let day_region = confirmedCase[2].children[0].data;
             
-                resolve(`${info}\n\n${text1}\n${text2}\n${text3}`);
+                let info = "누적 확진자 현황 : \`\`" + day_date + "\`\`";
+                let text1 = "신규 확진자 : \`\`" + day_all + "\`\`";
+                let text2 = "해외유입 : \`\`" + day_oversea + "\`\`";
+                let text3 = "국내발생 : \`\`" + day_region + "\`\`";
+
+
+                let oversea = response.body.split('DP_data.oversea.push').slice(1,8).map(x => x.split('("')[1].split('")')[0]); // 해외 유입
+                let region = response.body.split('DP_data.region.push').slice(1,8).map(x => x.split('("')[1].split('")')[0]); // 국내 발생
+                let date = response.body.split('DP_data.date.push').slice(1,8).map(x => x.split('("')[1].split('")')[0]); // 날짜
+                let covidList = `\`\`\`md\n`;
+                covidList += `# 코로나 기록\n`;
+                covidList += `[날짜][신규확진자] <해외유입 국내발생>\n`;
+                covidList += date.map((x, i) => {
+                    return `[2020.${x}][${parseInt(oversea[i])+parseInt(region[i])}] <${oversea[i]} ${region[i]}>`;
+                    // return `[2020.${x}][${((parseInt(oversea[i])+parseInt(region[i]))+'').padStart(3, ' ')}] <${oversea[i].padStart(7, ' ')} ${region[i].padStart(8, ' ')}>`;
+                    // return [x, region[i], date[i]]
+                }).join('\n')
+                covidList += `\`\`\``;
+            
+                resolve(`${info}\n\n${text1}\n${text2}\n${text3}\n\n${covidList}`);
             })
         });
     },
@@ -44,9 +63,9 @@ module.exports = {
             covid_notice_channel = bot.channels.cache.find(x => x.id == aa);
         }
 
-        covid_notice_channel.send(await module.exports.core());
+        covid_notice_channel.send(await module.exports.core(bot));
     },
     run: async (bot, message, args) => {
-        message.channel.send(await module.exports.core())
+        message.channel.send(await module.exports.core(bot))
     }
 }
